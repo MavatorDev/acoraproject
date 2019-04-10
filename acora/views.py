21295dd247cafbc2fa7bb7008399d9fa35573488
manuela <mvalenciat@eafit.edu.co>
2019-04-10 12:49:26 -0500

    subiendo cambios

--- acora/views.py
+++ acora/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import Equipo
 #crear vistas
 
 def post_list(request):
-    return render(request, 'acora/post_list.html',{})
+    equipos= Equipo.objects.order_by('puntaje')
+    return render(request, 'acora/post_list.html',{'equipos': equipos})
+
 
