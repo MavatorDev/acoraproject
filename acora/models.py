d1cc68554fcf401b1c2159c3ffc9323e0c702d65
manuela <mvalenciat@eafit.edu.co>
2019-04-17 08:26:11 -0500

    subiendo acto

--- acora/models.py
+++ acora/models.py
@@ -1,28 +1,18 @@
 from django.db import models
 from django.utils import timezone
+class Partida(models.Model):
+    codigo=models.charField(primary_key=True,max_length=20)
+    estado=models.BooleanField(default=False)
+    temporizador=models.TimeField(default=time.now)
 
 class Equipo(models.Model):
-   # id=models.CharField(max_length=200)
+    ide=models.CharField(primary_key=True,max_length=200)
+    nombre=models.CharField(unique=True)
     puntaje=models.IntegerField(default=0)
-   # timer= models.TimeField(default='00:00')
-    codigo=models.CharField(max_length=200)
-
-class Partida(models.Model):
-    codigo= models.CharField(max_length=200)
-
-class Pista(models.Model):
-   # id=models.CharField(max_length=200)
-    Actividad=models.CharField(max_length=300)
+    idPista=models.CharField()
+    codigo=models.ForeignKey(Partida)
 
 class Ranking(models.Model):
-   # id=models.CharField(max_length=20)
-    codigo=models.CharField(max_length=29)
-
-
-
-# def getTime(self):
- #       self.timer = time.now()
-  #      self.save()
-
-#def getpuntaje(self):
- #   return self.puntaje
+    idRanking=models.CharField()
+    codigo=models.ForeignKey(Partida)
+    
