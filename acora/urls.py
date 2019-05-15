6f1931fbae137fac840b641981aafb1eb99c6104
manuela <mvalenciat@eafit.edu.co>
2019-05-15 15:40:50 -0500

    manejo de errores y otras modificaciones

--- acora/urls.py
+++ acora/urls.py
@@ -8,18 +8,19 @@ from .views import actualizarPuntaje
 from .views import finalizarPartida
 from .views import getTemporizador
 from .views import getRanking
+from .views import CPartida
 from django.http import HttpResponse
 
 urlpatterns = [ 
-        path('equiposPartida/<int:pk>',equiposPartida.as_view(), name='equiposPartida'),
+        path('equiposPartida/<slug:foo>',equiposPartida.as_view(), name='equiposPartida'),
         path('crearPartida',crearPartida.as_view(), name='crearPartida'), 
         path('login/<slug:foo>',login.as_view(), name='login'),
         path('Clogin',Clogin.as_view(), name='Clogin'),
-        path('iniciarPartida/<int:pk>',iniciarPartida.as_view(), name='iniciarPartida'),
+        path('iniciarPartida/<slug:foo>',iniciarPartida.as_view(), name='iniciarPartida'),
         path('actualizarPuntaje/<slug:foo>',actualizarPuntaje.as_view(), name='actualizarPuntaje'),
-        path('finalizarPartida/<int:pk>',finalizarPartida.as_view(), name='finalizarPartida'),
-        path('getTemporizador/<int:pk>',getTemporizador.as_view(), name='getTemporizador'),
-        path('getRanking/<int:pk>',getRanking.as_view(), name='getRanking'),
-        path('CPartida/<int:pk>',CPartida.as_view(),name='CPartida'),
+        path('finalizarPartida/<slug:foo>',finalizarPartida.as_view(), name='finalizarPartida'),
+        path('getTemporizador/<slug:foo>',getTemporizador.as_view(), name='getTemporizador'),
+        path('getRanking/<slug:foo>',getRanking.as_view(), name='getRanking'),
+        path('CPartida/<slug:foo>',CPartida.as_view(), name='CPartida'),
         ]
 	
